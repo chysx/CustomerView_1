d9417166d4e936160b6acd32a1a605b2beb50473
chysx <976990321@qq.com>
2019-03-27 10:43:59 +0800

    优化工程结构

--- app/src/main/java/zhang/com/customerview_1/MainActivity.java
+++ app/src/main/java/zhang/com/customerview_1/MainActivity.java
@@ -1,11 +1,15 @@
 package zhang.com.customerview_1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import zhang.com.customerview_1.activity.PieActivity;
 import zhang.com.customerview_1.view.PieView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +19,20 @@ public class MainActivity extends AppCompatActivity {
         initView();
     }
 
+
     private void initView(){
-        PieView pieView = findViewById(R.id.pv);
+        Button btnPie = findViewById(R.id.btn_pie);
+        btnPie.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = null;
+        switch (view.getId()){
+            case R.id.btn_pie:
+                intent = new Intent(this, PieActivity.class);
+                break;
+        }
+        startActivity(intent);
     }
 }
